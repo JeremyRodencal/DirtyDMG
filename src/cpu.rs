09e17b34ac12f90ed4abbe0fcb1baca0664e2ffc
Jeremy Rodencal <jeremy.rodencal@alliancels.com>
2021-07-05 16:57:21 -0500

    Implmented most defined ops.
    Still needs tests and code to handle flags.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,7 +1,10 @@
+use std::num::Wrapping;
+use byteorder::{ByteOrder, LittleEndian};
 use crate::bus::{BusRW};
 
 #[allow(dead_code)]
 #[derive(Debug)]
+#[derive(Clone, Copy)]
 enum Register {
     A, F,
     B, C,
@@ -129,6 +132,109 @@ impl Cpu {
         self.reg.a = bus.bus_read8(addr);
         return self.reg.a
     }
+
+    fn execute_instruction(&mut self, bus:&mut impl BusRW, data: &[u8]) -> u8
+    {
+        let opcode = data[0];
+        let instruction = &INSTRUCTION_TABLE[opcode as usize];
+
+        self.execute_operation(bus, data, &instruction.op);
+        //TODO do something with cycle length
+
+        return instruction.cycles;
+    }
+
+    fn execute_operation(&mut self, bus:&mut impl BusRW, data: &[u8], op:&Operation)
+    {
+        use Operation::*;
+        match op {
+            Nop => (),
+            Stop => { panic!("Stop is not")},
+
+            // Move a register value into another register.
+            LdRR{dst, src} => { 
+                self.reg.write8(
+                    *dst, 
+                    self.reg.read8(*src)
+                );
+            },
+
+            // A register from memory transfer.
+            LdRM{dst, src} => {
+                self.reg.write8(
+                    *dst, 
+                    bus.bus_read8(
+                        self.reg.read16(*src) as usize
+                    )
+                );
+            },
+
+            // A 16bit immediate load into a 16 bit register.
+            LdR16I16{dst} => {
+                self.reg.write16(
+                    *dst, 
+                    LittleEndian::read_u16(&data[1..])
+                );
+            },
+
+            // Store a register into memory pointed to by 16bit register.
+            LdMR16{dst, src} => {
+                bus.bus_write8(
+                    self.reg.read16(*dst) as usize,
+                    self.reg.read8(*src)
+                );
+            },
+
+            // Load a register with an 8 bit immediate value.
+            LdRI{dst} => {
+                self.reg.write8(
+                    *dst,
+                    data[1]
+                );
+            },
+
+            // Save a 16 bit register to an immediate address.
+            LdI16R16{src} => {
+                bus.bus_write16(
+                    LittleEndian::read_u16(&data[1..]) as usize,
+                    self.reg.read16(*src)
+                );
+            },
+
+            // Increment a 16 bit register.
+            IncR16{dst} => {
+                let i = self.reg.read16(*dst).wrapping_add(1);
+                self.reg.write16(*dst, i);
+            },
+
+            // Increment an 8 bit register.
+            IncR{dst} => {
+                let i = self.reg.read8(*dst).wrapping_add(1);
+                self.reg.write8(*dst, i);
+            },
+            
+            // Decrement an 8 bit register
+            DecR{dst} => {
+                let i = self.reg.read8(*dst).wrapping_sub(1);
+                self.reg.write8(*dst, i);
+            },
+
+            // Decrement a 16 bit register
+            DecR16{dst} => {
+                let i = self.reg.read16(*dst).wrapping_sub(1);
+                self.reg.write16(*dst, i);
+            },
+
+            // // Rotate A once to the left, and carry bit 7 into the carry flag.
+            // Rlca,
+            // // Rotate A once to the right and carry bit 0 into the carry flag.
+            // Rrca,
+
+            // // Add two 16 bit registers.
+            // AddR16R16{dst:Register, src:Register},
+            _ => panic!("not implemented")
+        }
+    }
 }
 
 #[allow(dead_code)]
@@ -145,7 +251,7 @@ enum Operation {
     // A 16bit immediate load into a 16 bit register.
     LdR16I16{dst:Register},
     // Store a register into memory pointed to by 16bit register.
-    LdM16R{dst:Register, src:Register},
+    LdMR16{dst:Register, src:Register},
     // Load a register with an 8 bit immediate value.
     LdRI{dst:Register},
     // Save a 16 bit register to an immediate address.
@@ -184,7 +290,7 @@ const INSTRUCTION_TABLE: [Instruction;256] = [
     // 0x01 LD BC, d16
     Instruction{op:Operation::LdR16I16{dst:Register::BC}, length:3, cycles:3},
     // 0x02 LD (BC), A
-    Instruction{op:Operation::LdM16R{dst:Register::BC, src:Register::A}, length:1, cycles:2},
+    Instruction{op:Operation::LdMR16{dst:Register::BC, src:Register::A}, length:1, cycles:2},
     // 0x03 INC BC
     Instruction{op:Operation::IncR16{dst:Register::BC}, length: 1, cycles:2},
     // 0x04 INC B
