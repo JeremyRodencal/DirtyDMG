8ab05e085eee4d60f6875b850ca2a8d1b89f8475
Jeremy Rodencal <jeremy.rodencal@gmail.com>
2021-11-21 17:54:03 -0600

    Mapped PPU to bus in DMG.

--- src/dmg.rs
+++ src/dmg.rs
@@ -39,14 +39,16 @@ impl Dmg {
 
         // Map components to the bus.
         let mut bus = Bus::new();
-        bus.add_item(BusItem::new(0xC000, 0xDFFF, ram.clone()));
-        bus.add_item(BusItem::new(0xFF0F, 0xFF0F, isr.clone()));
-        bus.add_item(BusItem::new(0xFFFF, 0xFFFF, isr.clone()));
         bus.add_item(BusItem::new(0x0000, 0x7FFF, cart.clone()));
+        bus.add_item(BusItem::new(0x8000, 0x9FFF, ppu.clone()));
         bus.add_item(BusItem::new(0xA000, 0xBFFF, cart.clone()));
+        bus.add_item(BusItem::new(0xC000, 0xDFFF, ram.clone()));
         bus.add_item(BusItem::new(0xFF01, 0xFF02, stu.clone()));
-        bus.add_item(BusItem::new(0xFF80, 0xFFFE, zero_page.clone()));
         bus.add_item(BusItem::new(0xFF04, 0xFF07, tu.clone()));
+        bus.add_item(BusItem::new(0xFF0F, 0xFF0F, isr.clone()));
+        bus.add_item(BusItem::new(0xFF40, 0xFF4B, ppu.clone()));
+        bus.add_item(BusItem::new(0xFF80, 0xFFFE, zero_page.clone()));
+        bus.add_item(BusItem::new(0xFFFF, 0xFFFF, isr.clone()));
 
         let mut cpu = Cpu::new();
         cpu.reg.a = 0x01;
@@ -75,6 +77,7 @@ impl Dmg {
 
         self.cpu.handle_interrupts(&mut self.bus, &mut self.isr.as_ref().borrow_mut());
         let cycles = self.cpu.update(&mut self.bus);
+        self.ppu.as_ref().borrow_mut().execute_ticks(cycles as u16 * 4, &mut self.bus, &mut self.isr.as_ref().borrow_mut());
         {
             let mut stu = self.stu.as_ref().borrow_mut();
             stu.update(cycles as u32, &mut self.isr.as_ref().borrow_mut());
