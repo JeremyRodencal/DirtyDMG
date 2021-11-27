6eb5dccd175643afc95e2756eaed5cdb46c4b052
Jeremy Rodencal <jeremy.rodencal@gmail.com>
2021-11-27 16:54:28 -0600

    Created "interface" module.
    This shold contain the bits needed to handle platform specific
    rendering/sound/input/etc.
    
    Only has a definition for a "scanline" buffer right now.

---
+++ dirtydmg-core/src/interface.rs
@@ -0,0 +1,19 @@
+/// Structure used to hold gameboy graphics data.
+/// 
+/// Pixels are bitpacked within bytes, with each pixel being 2 bits long.
+/// Pixels are aranged in the bits such that the more significant the bits,
+/// the further right they are on the screen. The same applies to the byte
+/// Index, where higher values are farther right.
+pub struct ScanlineBuffer {
+    pub pixeldata: [u8;40]
+}
+
+impl ScanlineBuffer{
+    pub fn new() -> ScanlineBuffer{
+        ScanlineBuffer {
+            pixeldata: [0;40]
+        }
+    }
+}
+
+
